a39db055d3f262544bf843ab18320cfc825f802c
Andrey Burlakov <andrburl2@yandex.ru>
2020-08-24 21:20:49 +0300

    Updates

---
+++ precache-manifest.23d9f47cc7a3a34d85543eab67d041db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35880201f7c824e7a71c7b8a463b36aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feea5a3aa3e715775993",
+    "url": "/static/css/main.081bb88b.chunk.css"
+  },
+  {
+    "revision": "def15d53896a89850ad0",
+    "url": "/static/js/2.6b5e5bdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b5e5bdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feea5a3aa3e715775993",
+    "url": "/static/js/main.f468363d.chunk.js"
+  },
+  {
+    "revision": "5b2c3ee3bcc466bef490",
+    "url": "/static/js/runtime-main.837ceac1.js"
+  },
+  {
+    "revision": "7a0b256cd6b4e4b3780a479399081e4a",
+    "url": "/static/media/Exo2.0-Thin.7a0b256c.woff"
+  },
+  {
+    "revision": "f8cb1cae4c4dddae300712deb4783b04",
+    "url": "/static/media/cat.f8cb1cae.png"
+  },
+  {
+    "revision": "c7d794db3d179670847f9d46b9009c70",
+    "url": "/static/media/pattern.c7d794db.jpg"
+  }
+]);
